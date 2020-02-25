6eece9c289c5afc7ca3bdaf9ee7e7f1d6501d819
Kacper Zelichowski <kacper.zelichowski@sabre.com>
2020-02-25 11:25:54 +0100

    run with agent docker

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,5 +1,9 @@
 pipeline {
-    agent any
+    agent {
+        docker {
+            image 'adoptopenjdk/maven-openjdk11'
+        }
+    }
     stages {
         stage('ECHO ALL FILE NAMES') {
             steps {
