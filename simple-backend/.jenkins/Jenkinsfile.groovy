4f128fc20a3bdde136452cea6844d6be8193a15a
SG0307049 <mateusz.srednicki.ctr@sabre.com>
2020-02-25 11:22:10 +0100

    list files

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,10 +1,17 @@
 pipeline {
-	agent any
+	agent {
+		image 'adoptopenjdk/openjdk11'
+	}
 	stages {
 		stage('STAGE NAME'){
 			steps{
 				sh "ls -la"
 			}
 		}
+		stage('maven install'){
+			sh "cd .."
+			sh "maven clean install"
+		}
 	}
+
 }
