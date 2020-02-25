0ccf2d5d2f8deb50108db1c44260c740d7d12872
Agata Chucherko <Agata.Chucherko@sabre.com>
2020-02-25 11:28:31 +0100

    set directory

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -12,8 +12,9 @@ pipeline {
         }
         stage('Second') {
             steps{
-                sh "cd simple-backend/"
-                sh 'mvn clean install'
+                dir('simple-backend'){
+                    sh 'mvn clean install'
+                }
             }
         }
     }
