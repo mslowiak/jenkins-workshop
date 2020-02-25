454a22c8745bd90cb06e48b4aa80f75615771eb6
Agata Chucherko <Agata.Chucherko@sabre.com>
2020-02-25 11:23:57 +0100

    Add docker image

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,9 +1,19 @@
 pipeline {
-    agent any
+    agent{
+        docker{
+            image 'adoptopenjdk/maven-openjdk11'
+        }
+    }
     stages {
         stage('First') {
             steps{
                 sh "ls -la"
+            }
+        }
+        stage('Second') {
+            steps{
+                sh "cd simple-backend/"
+                sh 'mvn clean install'
             }
         }
     }
