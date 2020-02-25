284aa3a34b9a9459ea0483ec6fbca2c0b479e722
Dariusz Domanski <dariusz.domanski@sabre.com>
2020-02-25 11:29:01 +0100

    Create script to mvn clean install

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,11 +1,19 @@
 pipeline {
-    agent any
+    agent {
+        docker {
+            image 'adoptopenjdk/maven-openjdk11'
+        }
+    }
     stages {
         stage('TEST') {
             steps {
                 sh "ls -la"
+                dir('simple-backend') {
+                    sh 'mvn clean install'
+                }
             }
         }
     }
+
 
 }
