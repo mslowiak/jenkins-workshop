50e965802911f9d2763426626a6d877bb027a1c4
Michal Kobiec <Michal.Kobiec@sabre.com>
2020-03-04 15:19:59 +0100

    Add secrets

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -5,6 +5,7 @@ pipeline {
             args '-v /root/.m2:/root/.m2'
         }
     }
+    
     parameters {
         string(name: 'PRODUCT_NAME', defaultValue: 'Mr Jenkins', description: 'Name of the product?')
         choice(name: 'PROFILE', choices: ['local', 'dev', 'other'], description: 'Pick something')
@@ -17,7 +18,14 @@ pipeline {
             }
         }
         stage('run jar') {
+            environment {
+                PSWRD = credentials('PASSWORD')
+                USERNAME = credentials('USERNAME')
+                CLIENT_SECRET = credentials('CLIENT_SECRET')
+                CLIENT_ID  = credentials('CLIENT_ID ')
+            }
             steps {
+                sh "echo $env.PSWRD $env.USERNAME"
                 sh "cd simple-backend/target && java -jar app.jar --spring.profiles.active=$params.PROFILE --productName=$params.PRODUCT_NAME"
             }
         }
