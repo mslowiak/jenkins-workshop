2823ba331686c66c82eaa31f2815531e3d83c2b6
Agata Chucherko <Agata.Chucherko@sabre.com>
2020-03-04 15:01:26 +0100

    fix passing parameters

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -25,7 +25,7 @@ pipeline {
         stage('Run app'){
             steps{
                 dir('simple-backend/target'){
-                    sh 'java -jar app.jar --spring.profiles.active=$params.PROFILE --productName=$params.PRODUCT_NAME'
+                    sh "java -jar app.jar --spring.profiles.active=$params.PROFILE --productName=$params.PRODUCT_NAME"
                 }
             }
         }
