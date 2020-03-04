e1d0cf324e674b85ca0e4ff85a1248bfe38c6b19
Wiktoria Kurek <wiktoria.kurek.ctr@sabre.com>
2020-03-04 15:00:42 +0100

    add parameters

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -5,9 +5,16 @@ pipeline {
             args '-v /root/.m2:/root/.m2'
         }
     }
+    environment {
+        DEV_PASSWORD = credentials('PASSWORD')
+        DEV_USERNAME = credentials('USERNAME')
+        DEV_CLIENT_SECRET = credentials('CLIENT_SECRET')
+        DEV_CLIENT_ID = credentials('CLIENT_ID')
+    }
     parameters {
-        choice(name: 'PROFILE', choices: ['local', 'dev', 'other'], description: '')
+        choice(name: 'PROFILE', choices: ['local', 'dev', 'custom'], description: '')
         string(name: 'PRODUCT_NAME', defaultValue: 'dupa', description: '')
+        string(name: 'URL', defaultValue: '', description: '')
     }
     stages {
         stage('Stage one') {
@@ -22,10 +29,31 @@ pipeline {
                 }
             }
         }
-        stage('Stage three') {
+        stage('Run OTHER') {
+            when {
+                expression {
+                    PROFILE == 'custom'
+                }
+            }
             steps {
                 dir('simple-backend/target') {
-                    sh 'java -jar app.jar --spring.profiles.active=$params.PROFILE --productName=$params.PRODUCT_NAME'
+                    sh """java -jar app.jar --spring.profiles.active=$params.PROFILE --productName=$params.PRODUCT_NAME \
+                            --salesforce.username=$env.DEV_USERNAME --salesforce.password=$env.DEV_PASSWORD \
+                            --salesforce.clientId=$env.DEV_CLIENT_ID --salesforce.clientSecret=$env.DEV_CLIENT_SECRET \
+                            --salesforce.url=$params.URL
+                        """
+                }
+            }
+        }
+        stage('Run ') {
+            when {
+                expression {
+                    PROFILE != 'custom'
+                }
+            }
+            steps {
+                dir('simple-backend/target') {
+                    sh "java -jar app.jar --spring.profiles.active=$params.PROFILE --productName=$params.PRODUCT_NAME"
                 }
             }
         }
