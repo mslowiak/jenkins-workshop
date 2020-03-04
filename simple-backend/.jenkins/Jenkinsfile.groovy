fd4c18fcd48fdca62d07e3c0cdcf9bf4b1133d6f
SG0307049 <mateusz.srednicki.ctr@sabre.com>
2020-03-04 14:31:54 +0100

    args

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 pipeline {
 	agent {
 		docker{
-			image 'adoptopenjdk/openjdk11'
+			image 'adoptopenjdk/maven-openjdk11'
 			args '-v /root/.m2:/root/.m2'
 		}
 	}
@@ -14,7 +14,7 @@ pipeline {
 		stage('maven install'){
 			steps{
 				dir('simple-backend'){
-					sh "maven clean install"
+					sh "mvn clean install"
 				}
 				
 			}
