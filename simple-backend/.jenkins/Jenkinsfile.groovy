5c3ae0f88ea97da5ee0533b0603282ebc98feae3
Ania Czech <ania.czech@sabre.com>
2020-03-04 15:38:25 +0100

    new items

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
         stage('Deploy'){
             when {
                 expression {
-                    $params.PROFILE == 'other'
+                    params.PROFILE == 'other'
                 }
             }
             steps{
