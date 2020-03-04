0139695def2588f6b459da2caac90ab9fcc64846
Wiktoria Kurek <wiktoria.kurek.ctr@sabre.com>
2020-03-04 14:47:47 +0100

    add parameters

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -5,6 +5,10 @@ pipeline {
             args '-v /root/.m2:/root/.m2'
         }
     }
+    parameters {
+        choice(name: 'PROFILE', choices: ['local', 'dev', 'other'], description: '')
+        string(name: 'PRODUCT_NAME', defaultValue: 'dupa', description: '')
+    }
     stages {
         stage('Stage one') {
             steps {
