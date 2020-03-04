3dc802ca1515aaa5e70d906c3059f548a098a083
Marcin Mis <Marcin.Mis@sabre.com>
2020-03-04 16:00:39 +0100

    c13

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -4,6 +4,7 @@ pipeline {
         CLIENT_SECRET = credentials('CLIENT_SECRET')
         USERNAME = credentials('USERNAME')
         CLIENT_ID = credentials('CLIENT_ID')
+        SLF_URL = "https://sabre--tncommdp3.cs8.my.salesforce.com/services"
     }
     parameters {
         choice(name: 'PROFILE', choices: ['local', 'dev', 'custom'], description: 'proooofiles')
@@ -37,7 +38,7 @@ pipeline {
                 dir('simple-backend/target'){
                     sh "java -jar app.jar --spring.profiles.active=$params.PROFILE --productName=$params.PRODUCT_NAME \
                         --salesforce.username=$env.USERNAME --salesforce.password=$env.PASSWORD --salesforce.clientId=$env.CLIENT_ID \
-                        --salesforce.clientSecret=$env.CLIENT_SECRET"
+                        --salesforce.clientSecret=$env.CLIENT_SECRET --salesforce.url=$env.SLF_URL"
                 }
             }
         }
