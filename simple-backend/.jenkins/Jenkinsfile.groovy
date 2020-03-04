4b60e2a78c4827f4c80e3008d8f985bf9b61f84c
Dariusz Domanski <dariusz.domanski@sabre.com>
2020-03-04 14:39:00 +0100

    Create stage to run app

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -6,11 +6,18 @@ pipeline {
         }
     }
     stages {
-        stage('TEST') {
+        stage('BUILD') {
             steps {
                 sh "ls -la"
                 dir('simple-backend') {
                     sh 'mvn clean install'
+                }
+            }
+        }
+        stage('RUN_APP') {
+            steps {
+                dir('simple-backend/target') {
+                    sh "java -jar app.jar"
                 }
             }
         }
