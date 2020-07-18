fe065aa1412eb21b2b4cf96dd6837aadb0a67def
Bartlomiej Mont <bartlomiejmont@gmail.com>
2020-07-18 17:36:29 +0200

    add new branch

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,3 +1,21 @@
 pipeline {
-
+    agent {
+        docker {
+            image 'adoptopenjdk/maven-openjdk11'
+            args '-v /root/.m2:/root/.m2' // save cache to run faster 
+        }
+    }
+    stages {
+        stage('BUILD'){
+            steps {
+                sh 'echo "fobar"'
+                sh 'cd simple-backend && mvn clean install'
+            }
+        }
+        stage('RUN'){
+            steps{
+                sh 'cd simple-backend/target && java -jar app.jar'
+            }
+        }
+    }
 }
