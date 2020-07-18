c5e1c42e63129677c478912122df98d0af73ee16
Rafal Maj <rmaj91@gmail.com>
2020-07-18 17:41:24 +0200

    pipeline

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,3 +1,30 @@
 pipeline {
-
+    agent {
+        docker {
+            image 'adoptopenjdk/maven-openjdk11'
+            args '-v /root/.m2:/root/.m2'
+        }
+    }
+    stages {
+        stage('LIST FILES') {
+            steps {
+                sh 'ls -al'
+                sh 'pwd'
+            }
+        }
+        stage('BUILD') {
+            steps {
+                dir('simple-backend') {
+                    sh 'mvn clean install'
+                }
+            }
+        }
+        stage('RUN') {
+            steps {
+                dir('simple-backend/target') {
+                    sh 'java -jar app.jar --spring.profiles.active=DEV --service.url=http://07b8c8896d9a.ngrok.io'
+                }
+            }
+        }
+    }
 }
