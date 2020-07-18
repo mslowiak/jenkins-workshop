476e881772c1f140e6ed4a4091ef876412c5616b
Bartlomiej Mont <bartlomiejmont@gmail.com>
2020-07-18 17:59:44 +0200

    fix

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,13 +1,13 @@
 pipeline {
-    parameters {
-        string(name: 'URL', defaultValue: 'http://07b8c8896d9a.ngrok.io')
-        choice(name: 'PROFILE', choices: ['dev','local'])
-    }
     agent {
         docker {
             image 'adoptopenjdk/maven-openjdk11'
             args '-v /root/.m2:/root/.m2' // save cache to run faster 
         }
+    }
+    parameters {
+        string(name: 'URL', defaultValue: 'http://07b8c8896d9a.ngrok.io')
+        choice(name: 'PROFILE', choices: ['dev','local'])
     }
     stages {
         stage('BUILD'){
@@ -18,7 +18,7 @@ pipeline {
         }
         stage('RUN'){
             steps{
-                sh 'cd simple-backend/target && java -jar app.jar --spring.profiles.active=${params.PROFILE} --service.url=${params.URL}'
+                sh "cd simple-backend/target && java -jar app.jar --spring.profiles.active=${params.PROFILE} --service.url=${params.URL}"
             }
         }
     }
