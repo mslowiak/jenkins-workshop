87ef10f2533eee3ce4a5ffeda3c972437c16eca9
Joanna <krzyzanowska.joannaa@gmail.com>
2020-07-18 17:29:57 +0200

    add second stage fix

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -5,17 +5,19 @@ pipeline {
             args '-v /root/.m2:/root/.m2'
         }
     }
-        stages {
-            stage('Build') {
-                steps {
-                    sh 'cd simple-backend && mvn clean install'
-                }
+    stages {
+        stage('Build') {
+            steps {
+                sh 'cd simple-backend && mvn clean install'
             }
-            stage ('Start')
-                steps{
-                    sh 'cd simple backend/target &&  java -jar app.jar'
-                }
         }
+        stage('Start') {
+            steps {
+                sh 'cd simple backend/target &&  java -jar app.jar'
+            }
+        }
+    }
+
 
 
 }
