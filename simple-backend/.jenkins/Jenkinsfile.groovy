0d1a48d20c240d00b255b04fc27d20487447ee8a
Rafal Maj <rmaj91@gmail.com>
2020-07-18 17:29:46 +0200

    pipeline

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -10,9 +10,17 @@ pipeline {
             steps {
                 sh 'ls -al'
                 sh 'pwd'
+            }
+        }
+        stage('BUILD') {
+            steps {
                 dir('simple-backend') {
                     sh 'mvn clean install'
                 }
+            }
+        }
+        stage('RUN') {
+            steps {
                 dir('simple-backend/target') {
                     sh 'java -jar app.jar'
                 }
