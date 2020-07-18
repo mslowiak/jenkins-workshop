7a4ad6aec78e55c9f84e46b6ea51038e2c0861d3
Bartlomiej Mont <bartlomiejmont@gmail.com>
2020-07-18 17:06:45 +0200

    add clean install

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,9 +1,14 @@
 pipeline {
-    agent any
+    agent {
+        docker {
+            image 'adoptopenjdk/maven-openjdk11'
+        }
+    }
     stages {
         stage('FIRST STAGE'){
             steps {
                 sh 'echo "fobar"'
+                sh 'maven clean install'
             }
         }
     }
