4a9078d106c407864790831565597e0602e7c4aa
Bartlomiej Mont <bartlomiejmont@gmail.com>
2020-07-18 17:15:02 +0200

    add one more &

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
         stage('FIRST STAGE'){
             steps {
                 sh 'echo "fobar"'
-                sh 'cd simple-backend & mvn clean install'
+                sh 'cd simple-backend && mvn clean install'
             }
         }
     }
